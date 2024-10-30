2ad452b01b31622432177c5c0a575918275b3045
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-30 12:41:59 -0500

    Update problema1.cpp

--- problema1.cpp
+++ problema1.cpp
@@ -1,3 +1,8 @@
+
+//Integrantes:
+//1. Olivares Sarmiento, Javier Ignacio Leon
+//2. Silva Reyes, Santiago Miguel
+//3. Meneses Roncal, Matias Alonso
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
